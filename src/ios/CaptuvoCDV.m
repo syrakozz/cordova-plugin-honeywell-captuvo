0785c6d19d65778e83cb6f05c44c14f00fed0399
syrakozz <syrakozz@users.noreply.github.com>
2016-05-09 17:18:57 +0300

    Merge changes to allow for sending callback multiple times

--- src/ios/CaptuvoCDV.m
+++ src/ios/CaptuvoCDV.m
@@ -59,7 +59,8 @@
     CDVPluginResult *result = [CDVPluginResult
                                resultWithStatus:CDVCommandStatus_OK
                                messageAsString:data];
-    
+    // keep the callback valid.
+    [result setKeepCallbackAsBool:YES];
     [self.commandDelegate sendPluginResult:result callbackId:self.scannerCallbackId];
 }
 
@@ -139,6 +140,8 @@
                                resultWithStatus: status ? CDVCommandStatus_OK : CDVCommandStatus_ERROR
                                messageAsString:data];
     
+    // keep the callback valid.
+    [result setKeepCallbackAsBool:YES];
     [self.commandDelegate sendPluginResult:result callbackId:self.msrCallbackId];
 }
 -(void)msrReady{
@@ -160,10 +163,12 @@
         //if didn't read status, send an error
         BOOL didRead = newBatteryStatus != BatteryStatusUndefined;
         
+
         CDVPluginResult *result = [CDVPluginResult
                                    resultWithStatus: didRead ? CDVCommandStatus_OK : CDVCommandStatus_ERROR
                                    messageAsInt:status];
-        
+        // keep the callback valid.
+        [result setKeepCallbackAsBool:YES];
         [self.commandDelegate sendPluginResult:result callbackId:self.batteryCallbackId];
     }
 }
